4a1fdc31ba595c48e030f98a031753abe4e72630
FelipeRa3002 <136349846+FelipeRa3002@users.noreply.github.com>
2024-09-08 16:47:01 -0300

    Add files via upload

---
+++ Alura/ImersaoDevcomGoogleGemini/javascript/dados.js
@@ -0,0 +1,136 @@
+
+let dados = [
+    {
+        titulo: "Makima",
+        subTitulo:"Makima: Um enigma envolto em mistério",
+        descricao: "Makima é uma das personagens mais complexas e fascinantes do mangá e anime Chainsaw Man. Inicialmente apresentada como uma líder carismática e misteriosa da Segurança Pública, responsável por caçar demônios, Makima logo revela uma faceta muito mais sombria e manipulativa.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Makima",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio  Controle Quatro Cavaleiros Nayuta",
+        imagem: "https://i.pinimg.com/originals/94/fe/e9/94fee9331bd0f4908cb43cbe22f1936f.jpg",
+        cor:"red",
+        video:"https://www.youtube.com/watch?v=TTTpT0nzeEk"
+    },
+    {
+        titulo: "Power",
+        subTitulo:"Power: A Demônia da Sangue com um coração (quase) de ouro",
+        descricao: "Power é uma das personagens mais memoráveis e cativantes de Chainsaw Man. Como uma demônia do sangue, ela exibe uma personalidade explosiva, impulsiva e extremamente violenta. No entanto, por trás dessa fachada agressiva, esconde um lado vulnerável e uma profunda necessidade de conexão.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Power",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Infernal Sangue Possessa",
+        imagem: "https://c.wallhere.com/photos/4f/1b/Chainsaw_Man_TatsukiFujimoto_MAPPA_anime_Power_Chainsaw_Man_4K_anime_girls-2176231.jpg!d",
+        cor:"Orange",
+        video:"https://www.youtube.com/watch?v=PXGkTgVgbrQ"
+    },
+    {
+        titulo: "Himeno",
+        subTitulo:"Himeno: A veterana marcada pela tragédia",
+        descricao: "Himeno é uma personagem marcante em Chainsaw Man, que, apesar de ter um tempo de tela relativamente curto, deixa uma profunda impressão nos fãs. Ela é uma caçadora de demônios experiente, integrante da Divisão 4 da Segurança Pública, e serve como mentora para o protagonista, Denji.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Himeno",
+        tags: "Chainsaw Man ChainsawMan Humano Humana Fumante",
+        imagem: "https://th.bing.com/th/id/OIP.y9ufDWvTJ52xBEpRhTChDwHaEK?rs=1&pid=ImgDetMain",
+        cor:"black",
+        video:"https://www.youtube.com/watch?v=aDQj8cTFXD8"
+    },
+    {
+        titulo: "Kobeni Higashiyama",
+        subTitulo:"Kobeni Higashiyama: A novata aterrorizada",
+        descricao: "Kobeni Higashiyama é uma das personagens mais memoráveis de Chainsaw Man, introduzida como uma novata na Divisão de Segurança Pública. Apesar de sua aparência frágil e personalidade tímida, Kobeni é uma jovem determinada a sobreviver em um mundo repleto de demônios.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Kobeni_Higashiyama",
+        tags: "Chainsaw Man ChainsawMan Humano Humana Louca",
+        imagem: "https://story.agar.kr/files/skins/j8E.webp",
+        cor:"black",
+        video:"https://www.youtube.com/watch?v=MtlVBUxORW8"
+    },
+    {
+        titulo: "Angel Devil",
+        subTitulo:"Angel Devil: A Contradição Encarnada",
+        descricao: "Angel Devil é uma personagem complexa e intrigante em Chainsaw Man. Apresentada inicialmente como um demônio arrogante e egocêntrico, com uma aparência angelical irônica, ela rapidamente revela camadas mais profundas de sua personalidade.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Angel_Devil",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Anjo",
+        imagem: "https://sportshub.cbsistatic.com/i/2022/12/21/c66bfabe-9517-4595-9279-47ff1442b38c/chainsaw-man-angel-devil.jpg?auto=webp&width=1210&height=681&crop=1.777:1,smart",
+        cor:"#FEE995",
+        video:"https://www.youtube.com/watch?v=lWc02vur1fc"
+    },
+    {
+        titulo: "Quanxi",
+        subTitulo:"Quanxi: A Bruxa da Guerra",
+        descricao: "Quanxi é uma personagem marcante em Chainsaw Man, conhecida por sua beleza, força e passado misterioso. Ela é introduzida como uma poderosa caçadora de demônios, com uma personalidade complexa e um apetite insaciável por poder.",
+        link: "https://liberproeliis.fandom.com/pt-br/wiki/Quanxi_(Chainsaw_Man)",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Humano Humana Hibridos Híbridos Besta Guerra",
+        imagem: "https://i.pinimg.com/736x/f1/0b/1d/f10b1dbf9e2ea57ae22446e397b14bdf.jpg",
+        cor:"Gray",
+        video:"https://www.youtube.com/watch?v=sMJsVESk6kk"
+    }
+    ,
+    {
+        titulo: "Princi",
+        subTitulo:"Princi: A Caçadora de Demônios Aranha",
+        descricao: "Princi é uma personagem intrigante e poderosa em Chainsaw Man. Ela é a Demônio Aranha, uma caçadora de demônios que trabalha para a Divisão Especial 4 de Tóquio. Apesar de sua aparência humana, ela possui oito pernas afiadas que se escondem sob suas roupas, revelando sua verdadeira natureza demoníaca.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Princi",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Aranha",
+        imagem: "https://th.bing.com/th/id/OIP.Yx5nwGlwCNgXX7qHivJMBQHaHI?rs=1&pid=ImgDetMain",
+        cor:"Brown",
+        video:"https://www.youtube.com/watch?v=9Jaojq6DFIo"
+    }
+    ,
+    {
+        titulo: "Reze",
+        subTitulo:"Reze: A Bomba Humana de Chainsaw Man",
+        descricao: "Reze é uma personagem icônica e complexa do anime e mangá Chainsaw Man. Conhecida como o Demônio Bomba, ela é uma assassina enviada para eliminar Denji, o protagonista. Reze é famosa por sua aparência angelical e personalidade sedutora, que contrastam com sua natureza explosiva e violenta.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Reze",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Bomba Humano Humana Hibridos Híbridos Rato Campo",
+        imagem: "https://staticc.sportskeeda.com/editor/2023/04/5d25e-16823248539982-1920.jpg",
+        cor:"purple",
+        video:"https://www.youtube.com/watch?v=ZNfl1pxLqtI"
+    },
+    {
+        titulo: "Santa Claus",
+        subTitulo:"Santa Claus em Chainsaw Man: Uma Visão Geral",
+        descricao: "Santa Claus em Chainsaw Man é uma personificação sinistra e inesperada de um ícone natalino. Ao invés da figura alegre e bondosa que distribui presentes, a Santa Claus do mangá é uma manipuladora e poderosa caçadora de demônios, com um passado obscuro e uma personalidade complexa.",
+        link: "https://chainsaw-man.fandom.com/wiki/Santa_Claus",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Natal papai noel",
+        imagem: "https://th.bing.com/th/id/OIP.IgHkXL3sqjaUyvbOIf4OaQHaHa?rs=1&pid=ImgDetMain",
+        cor:"#E2282A",
+        video:"https://www.youtube.com/watch?v=6pWezkLxalI&list=RD6pWezkLxalI&start_radio=1&rv=6pWezkLxalI&t=0"
+    },
+    {
+        titulo: "Akane Sawatari",
+        subTitulo:"Akane Sawatari: Uma Caçadora de Demônios Complexa",
+        descricao: "Akane Sawatari é uma personagem marcante em Chainsaw Man. Com sua aparência jovem e habilidades excepcionais, ela rapidamente se destaca como uma caçadora de demônios talentosa e dedicada.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Akane_Sawatari",
+        tags: "Chainsaw Man ChainsawMan Humano Humana",
+        imagem: "https://theenvoyweb.com/wp-content/uploads/2022/12/akane-sawatari-chainsaw-man.jpg",
+        cor:"#C9B977",
+        video:"https://www.youtube.com/watch?v=eUjuZRZrhQE"
+    },
+    {
+        titulo: "Yoru",
+        subTitulo:"Yoru: A Assassina com um Coração (Quase) Gelado",
+        descricao: "Yoru é uma das personagens mais complexas e intrigantes de Chainsaw Man. Inicialmente apresentada como uma assassina fria e calculista, com um passado obscuro e uma missão singular, Yoru revela camadas de profundidade que surpreendem os leitores.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Yoru",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio Guerra Quatro Cavaleiros Asa Mitaka Infernal",
+        imagem: "https://th.bing.com/th/id/OIP.jKzHZQykSSU556ZQtWxPmQAAAA?rs=1&pid=ImgDetMain",
+        cor:"#E70607",
+        video:"https://www.youtube.com/watch?v=pumDq2UwFlo"
+    },
+    {
+        titulo: "Asa Mitaka",
+        subTitulo:"Asa Mitaka: Uma estudante comum",
+        descricao: "Asa Mitaka é uma personagem complexa e intrigante introduzida na segunda parte de Chainsaw Man. Inicialmente, é apresentada como uma estudante comum, mas logo revela sua verdadeira natureza como o Demônio da Guerra (Guerra Devil)",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Asa_Mitaka",
+        tags: "Chainsaw Man ChainsawMan Humano Humana Guerra Quatro Cavaleiros Yoru Infernal",
+        imagem: "https://tierragamer.com/wp-content/uploads/2023/02/5AsaMitakatierragamer-1024x576.jpg",
+        cor:"#693D2C",
+        video:"https://www.youtube.com/watch?v=R2kDu0GPM3o"
+    },   {
+        titulo: "Nayuta",
+        subTitulo:"Nayuta: reencarnação do Demônio do Controle",
+        descricao: "Nayuta é uma personagem introduzida na segunda parte de Chainsaw Man. Ela é a reencarnação do Demônio do Controle, que anteriormente habitava o corpo de Makima. Apesar de ter herdado os poderes de Makima, Nayuta apresenta uma personalidade e um desenvolvimento bem diferentes.",
+        link: "https://chainsaw-man.fandom.com/pt-br/wiki/Nayuta",
+        tags: "Chainsaw Man ChainsawMan Demônio Demonio  Controle Quatro Cavaleiros Makima",
+        imagem: "https://th.bing.com/th/id/OIP.0arIbUQiGR8saBKBC65r-AHaND?rs=1&pid=ImgDetMain",
+        cor:"#D88E1D",
+        video:"https://www.youtube.com/shorts/9xbYRebPkys"
+    }
+
+
+];
---
+++ Alura/ImersaoDevcomGoogleGemini/javascript/dadosjujutsu.js
@@ -0,0 +1,129 @@
+let dados = [
+    {
+        titulo: "Kasumi Miwa",
+        subTitulo:"Kasumi Miwa: Uma Feiticeira em Formação",
+        descricao: "Kasumi Miwa é uma personagem de Jujutsu Kaisen que, apesar de não ter técnicas de maldição inatas tão poderosas quanto outros feiticeiros, compensa com sua determinação e trabalho duro.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Kasumi_Miwa",
+        tags: "Jujutsu Kaisen JujutsuKaisen Feiticeira Estudante Kyoto Pobre Humana Humano",
+        imagem: "https://i.pinimg.com/originals/60/c4/52/60c452c8f15b6bc4512bf5da77f0c2bc.jpg",
+        cor:"#0B84D0",
+        video:"https://www.youtube.com/watch?v=hUV5fOpdC_M"
+    },{
+        titulo: "Kaori(Kenjaku)",
+        subTitulo:"Kaori Itadori: Uma Figura Enigmática em Jujutsu Kaisen",
+        descricao: "Kaori Itadori é uma personagem de Jujutsu Kaisen que, apesar de ter poucas aparições diretas, desempenha um papel crucial na história de Yuji Itadori. Inicialmente apresentada como a mãe de Yuji, sua verdadeira identidade e motivações revelaram-se mais complexas e sombrias ao longo da trama.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Kenjaku",
+        tags: "Jujutsu Kaisen JujutsuKaisen Kenjaku Mãe Mae",
+        imagem: "https://dcimg2.dcinside.co.kr/viewimage.php?id=27b2df35eadd34&no=24b0d769e1d32ca73cea8efa11d02831743ce42f276e54fea8bfbfa89b5d18d753063146edc99fa91fce170f170a6d0a6a9a4cecd41f921e50b2b4786df2f6d0e79912e00c72d3c60b3fb68185ff110ed5e94112c3f8a040307ef55b8fd9b5628fbfd9e9a663",
+        cor:"Black",
+        video:"https://www.youtube.com/watch?v=YQZUUKSX04I"
+    },{
+        titulo: "Mai Zenin",
+        subTitulo:"Mai Zenin: Uma Tragédia Familiar em Jujutsu Kaisen",
+        descricao: "Mai Zenin é uma personagem complexa e trágica do universo de Jujutsu Kaisen. Nascida em um clã de feiticeiros poderosos, ela era vista como inferior por não possuir uma energia amaldiçoada excepcional, como sua irmã gêmea, Maki.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Mai_Zenin",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Cla Clã Irmã Irma Gêmea Gemea Feiticeira Estudante Kyoto",
+        imagem: "https://i.pinimg.com/736x/5a/00/80/5a0080cb4b5432f4761633b5806f991c.jpg",
+        cor:"#34A806",
+        video:"https://www.youtube.com/watch?v=rMl996kK4HQ"
+    },{
+        titulo: "Maki Zenin",
+        subTitulo:"Maki Zenin: A Feiticeira Que Desafiou as Expectativas",
+        descricao: "Maki Zenin é uma personagem marcante em Jujutsu Kaisen. Apesar de pertencer ao poderoso Clã Zenin, um dos três grandes clãs de feiticeiros jujutsu, Maki possui uma característica única: ela nasceu praticamente sem energia amaldiçoada.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Maki_Zenin",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Cla Clã Irmã Irma Gêmea Gemea Feiticeira Estudante Tóquio Toquio Tokyo Restrição Restricao Celestial",
+        imagem: "https://th.bing.com/th/id/OIP.yCb7yPRWxaQ9HBbpotOEywAAAA?rs=1&pid=ImgDetMain",
+        cor:"#275415",
+        video:"https://www.youtube.com/watch?v=XF_2i25ugwc"
+    },{
+        titulo: "Mei Mei",
+        subTitulo:"Mei Mei: A Feiticeira Mercenária de Jujutsu Kaisen",
+        descricao: 'Mei Mei é uma personagem marcante em Jujutsu Kaisen. Conhecida como "A Escritora Amaldiçoada", ela é uma feiticeira de alto nível, afiliada ao Clã Zenin, que se destaca por sua técnica única e personalidade peculiar.',
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Mei_Mei",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Irmã Irma Feiticeira Grau 1 Primeiro Classe Mercenária Mercenaria",
+        imagem: "https://criticalhits.com.br/wp-content/uploads/2023/04/latest-1-1-910x512.jpg",
+        cor:"#B3E9EC",
+        video:"https://www.youtube.com/watch?v=DtoVdwGt3rc"
+    },{
+        titulo: "Mimiko Hasaba",
+        subTitulo:"Mimiko Hasaba: Uma Mente Estratégica em Jujutsu Kaisen",
+        descricao: "Mimiko Hasaba é uma personagem secundária da série de mangá e anime Jujutsu Kaisen. Apesar de não ter um papel central na trama, ela se destaca como uma estudante inteligente e analítica da Kyoto Jujutsu High.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Mimiko_Hasaba",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Irmã Irma Gêmea Gemea Geto Estudante Kyoto",
+        imagem: "https://i.pinimg.com/736x/50/70/69/507069a85a1cd55862dacfa11fae109a.jpg",
+        cor:"#422600",
+        video:"https://www.youtube.com/watch?v=sDJ4J8EfqRM"
+    },{
+        titulo: "Nanako Hasaba",
+        subTitulo:"Nanako: Uma Bruxa Tecnológica em Jujutsu Kaisen",
+        descricao: "Nanako é uma personagem intrigante e poderosa introduzida em Jujutsu Kaisen. Adotada por Geto Suguru, ela exibe uma habilidade única que combina elementos de tecnologia e jujutsu.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Nanako_Hasaba",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Irmã Irma Gêmea Gemea Geto Estudante Kyoto",
+        imagem: "https://i.pinimg.com/originals/ee/9e/25/ee9e25f8a4ae1d3a60ec16dd39820638.jpg",
+        cor:"#C2B16E",
+        video:"https://www.youtube.com/watch?v=Rnls7jSkZJA"
+    },{
+        titulo: "Utahime Iori",
+        subTitulo:"Utahime Iori: Uma Feiticeira Experiente e dedicada em Jujutsu Kaisen",
+        descricao: "Utahime Iori é uma personagem importante em Jujutsu Kaisen, atuando como professora no Colégio Técnico de Maldições de Kyoto. Ela é uma feiticeira jujutsu extremamente poderosa e experiente, conhecida por sua natureza séria e dedicada.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Utahime_Iori",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Feiticeira Professora Kyoto",
+        imagem: "https://i.pinimg.com/736x/4b/0e/ae/4b0eae12645ca2b74dbdd2e24a432660.jpg",
+        cor:"#423F94",
+        video:"https://www.youtube.com/watch?v=hgLhX8I3LH8"
+    },{
+        titulo: "Yuki Tsukumo",
+        subTitulo:"Yuki Tsukumo: A Feiticeira de Grau Especial e Misteriosa de Jujutsu Kaisen",
+        descricao: 'Yuki Tsukumo é uma figura enigmática e poderosa no universo de Jujutsu Kaisen. Classificada como uma Feiticeira de Grau Especial, ela possui habilidades únicas e um passado nebuloso que a tornam uma das personagens mais intrigantes da série.<br> <br> Qual é o seu tipo de mulher?',
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Yuki_Tsukumo",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Feiticeira Grau Especial Classe",
+        imagem: "https://criticalhits.com.br/wp-content/uploads/2023/08/yuki-tsukumo-personality-and-app-1280x640.jpg",
+        cor:"#F5DA71",
+        video:"https://www.youtube.com/watch?v=_JrH8fgF8lY"
+    },{
+        titulo: "Yuko Ozawa",
+        subTitulo:"Yuko Ozawa: Uma Namorada do Itadori?",
+        descricao: "Yuko Ozawa é uma personagem secundária em Jujutsu Kaisen. Ela foi colega de classe de Yuji Itadori na escola secundária.<br><br>Uma garota que gosta do Itadori.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Yuko_Ozawa",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano",
+        imagem: "https://staticg.sportskeeda.com/editor/2023/09/d4b28-16935494973321-1920.jpg",
+        cor:"#422600", /* Cor castanho */
+        video:"https://www.youtube.com/shorts/lChRgmkhNb8"
+    },{
+        titulo: "Shoko Ieiri",
+        subTitulo:"Shoko Ieiri: A Feiticeira Médica de Jujutsu Kaisen",
+        descricao: 'Shoko Ieiri é um personagem da série Jujutsu Kaisen. Ela é uma ex-aluna do Tokyo Jujutsu High, tendo sido colega de classe de Satoru Gojo e Suguru Geto. Depois de se formar em um feiticeiro de jujutsu de pleno direito, Shoko se tornou o médico primário da escola.',
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Shoko_Ieiri",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Feiticeira Médico Médica Medico Medica Fumante",
+        imagem: "https://i.pinimg.com/736x/47/4b/bb/474bbb9bd7112e609a03706b546d4862.jpg",
+        cor:"#E7DDFF",
+        video:"https://www.youtube.com/shorts/39pXT9_eSUI"
+    },{
+        titulo: "Nobara Kugisaki",
+        subTitulo:"Nobara Kugisaki: A Bruxa Rústica de Jujutsu Kaisen",
+        descricao: "Nobara Kugisaki é uma das protagonistas de Jujutsu Kaisen, uma jovem bruxa que, apesar de sua aparência doce e exterior confiante, possui uma força interior inabalável e um espírito competitivo. Originária de uma área rural, Nobara buscava uma vida mais agitada na cidade grande e encontrou seu caminho na Escola Técnica de Jujutsu de Tóquio.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Nobara_Kugisaki",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Feiticeira Estudante Tóquio Toquio Tokyo",
+        imagem: "https://i0.wp.com/awsmone.com/wp-content/uploads/2023/07/Nobara-Kugisaki.webp",
+        cor:"#422600",
+        video:"https://www.youtube.com/watch?v=oJWtbjgCw84"
+    },{
+        titulo: "Tsumiki Fushiguro",
+        subTitulo:"Tsumiki Fushiguro: Uma Revelação Chocante em Jujutsu Kaisen",
+        descricao: "Tsumiki Fushiguro é a irmã mais nova de Megumi Fushiguro e um dos personagens mais intrigantes de Jujutsu Kaisen. Inicialmente apresentada como uma jovem comum, presa no perigoso Jogo do Abate, sua verdadeira identidade foi revelada de forma chocante, mudando completamente a dinâmica da história.",
+        link: "https://jujutsukaisen.fandom.com/pt-br/wiki/Yuko_Ozawa",
+        tags: "Jujutsu Kaisen JujutsuKaisen Humana Humano Irmã Irma",
+        imagem: "https://th.bing.com/th/id/R.2fe068753f04f166ed27a5e1a0fa515e?rik=QZVCKnpept8xfg&riu=http%3a%2f%2fpm1.aminoapps.com%2f8260%2f3b17813c279bbcefe8976fd976a64b34ec81871cr1-736-736v2_uhq.jpg&ehk=8nWUzv011FulL9P7J3m%2bn0i%2fg8mUaw0sm%2fYMqRQ%2flcw%3d&risl=&pid=ImgRaw&r=0",
+        cor:"#85520C",
+        video:"https://www.youtube.com/shorts/AAIZCJp0zg0"
+    },{
+        titulo: "Uraume",
+        subTitulo:"Uraume: A Leal Seguidora de Sukuna",
+        descricao: "Uraume é um personagem complexo e enigmático de Jujutsu Kaisen. Inicialmente apresentada como uma seguidora devotada do Rei das Maldições, Ryomen Sukuna, sua verdadeira natureza se revela gradualmente ao longo da história.",
+        link: "https://criticalhits.com.br/anime/uraume-jujutsu-kaisen/",
+        tags: "Jujutsu Kaisen JujutsuKaisen Maldição Maldições Maldicao Maldicoes",
+        imagem: "https://th.bing.com/th/id/OIP.MlIav12sW4Rp5MDdfuoKnAHaEK?rs=1&pid=ImgDetMain",
+        cor:"#75F8FE",
+        video:"https://www.youtube.com/shorts/4YtV7TLO0Vg"
+    }
+]
