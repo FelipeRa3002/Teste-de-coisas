0f0e23cf9715c276871c1181266612b4499b253d
Felipe Rangel <136349846+FelipeRa3002@users.noreply.github.com>
2025-12-28 09:48:02 -0300

    Refactor app.js to improve product addition logic

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,20 +1,21 @@
 let totalGeral
 limpar()
+alert("oi")
 function adicionar(){
     let Produto = document.getElementById("produto").value
     let nomeProduto = Produto.split("-")[0]
     let valorUnitario = Produto.split("R$")[1]
     let quantidade = document.getElementById("quantidade").value
     if ( quantidade > 0 ){
-    let preco = quantidade*valorUnitario
-    let listaProdutos = document.getElementById("lista-produtos")
-    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-  </section>`
-    totalGeral = totalGeral + preco
-    let campoTotal = document.getElementById("valor-total")
-    campoTotal.textContent =  `R$ ${totalGeral}`
-    document.getElementById("quantidade").value = 1
+        let preco = quantidade*valorUnitario
+        let listaProdutos = document.getElementById("lista-produtos")
+        listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+      </section>`
+        totalGeral = totalGeral + preco
+        let campoTotal = document.getElementById("valor-total")
+        campoTotal.textContent =  `R$ ${totalGeral}`
+        document.getElementById("quantidade").value = 1
     } else{
         alert(`Não foi possível adicionar o "${nomeProduto}" porque a quantidade selecionada é zero.`)
     }
@@ -25,3 +26,4 @@ function limpar(){
     document.getElementById("valor-total").innerHTML = "R$ 0,00"
 
 }
+
