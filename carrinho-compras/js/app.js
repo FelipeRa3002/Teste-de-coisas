95c7fc31ee1b4068f99a900d3574948b3f144b96
Felipe Rangel <136349846+FelipeRa3002@users.noreply.github.com>
2025-12-28 09:42:04 -0300

    Update app.js

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -5,6 +5,7 @@ function adicionar(){
     let nomeProduto = Produto.split("-")[0]
     let valorUnitario = Produto.split("R$")[1]
     let quantidade = document.getElementById("quantidade").value
+    if ( quantidade > 0 ){
     let preco = quantidade*valorUnitario
     let listaProdutos = document.getElementById("lista-produtos")
     listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
@@ -14,9 +15,13 @@ function adicionar(){
     let campoTotal = document.getElementById("valor-total")
     campoTotal.textContent =  `R$ ${totalGeral}`
     document.getElementById("quantidade").value = 1
+    } else{
+        alert(`Não foi possível adicionar o "${nomeProduto}" porque a quantidade selecionada é zero.`)
+    }
 }
 function limpar(){
     totalGeral = 0
     document.getElementById("lista-produtos").innerHTML = ""
     document.getElementById("valor-total").innerHTML = "R$ 0,00"
+
 }
